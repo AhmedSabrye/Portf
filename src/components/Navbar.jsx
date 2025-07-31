3e604e0000957d06d29015793940cd3b61d05190
Ahmed Sabry <161619341+AhmedSabrye@users.noreply.github.com>
2025-07-31 10:22:45 +0300

    cv link updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,13 @@ import { Link } from "react-scroll";
 export default function Navbar() {
   return (
     <nav className="z-40 rounded-2xl shadow-lg mb-12 max-w-7xl mx-auto sticky top-8 bg-white  flex items-center justify-between px-6 lg:px-10 py-6">
-      <Link to="home" smooth={true} offset={-100} duration={750} className="text-xs sm:text-sm font-extrabold uppercase bg-primary cursor-pointer text-white px-4 py-2 rounded-full shadow-lg">
+      <Link
+        to="home"
+        smooth={true}
+        offset={-100}
+        duration={750}
+        className="text-xs sm:text-sm font-extrabold uppercase bg-primary cursor-pointer text-white px-4 py-2 rounded-full shadow-lg"
+      >
         Sabry
       </Link>
       <ul className="hidden md:flex gap-8 font-medium text-gray-600">
@@ -53,7 +59,7 @@ export default function Navbar() {
         </li>
       </ul>
       <a
-        href="https://drive.google.com/file/d/1fPRWH9bKz_vwETeCyFAZdi4v9FUr-90U/view?usp=sharing"
+        href="https://drive.google.com/file/d/1oIV3TafB7PvltBrofKdA1FFy-23s_3gf/view?usp=sharing"
         target="_blank"
         className="text-xs sm:text-sm font-semibold bg-gray-900 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg transition"
       >
