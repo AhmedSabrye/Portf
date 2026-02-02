f71a9f4920caa011b237ffda8aaabb14f7149a3f
AhmedSabrye <ahmedsabry.fr@gmail.com>
2026-02-02 09:39:34 +0200

    build: regenerate Slidev presentation assets and update HTML entry points.

--- public/Slides/GDG-HTML-CSS-Basics/assets/slidev/default-DRAzkwpE.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-BRJ1ipaS.js";import{_ as r}from"../index-BtxRqr-T.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
